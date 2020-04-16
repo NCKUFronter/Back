3f12bba2726eb0f81700793975496a8c4d70fe6b
WishChen <tp6jo4vm4@gmail.com>
2020-04-16 21:11:55 +0800

    change /record to router and get module

---
+++ routes/record.js
@@ -0,0 +1,78 @@
+var express = require("express")
+var router = express.Router();
+//var db = require('../models/db')
+
+// mongoDB init
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://uidd:1111@uidd-cluster-8k7z8.mongodb.net/uidd-db?retryWrites=true&w=majority";
+const client = MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if (err) throw err;
+});
+
+
+// GET from database 
+router.get('/', function (req, res) {
+    const collection = client.db("uidd-db").collection("uidd");
+    collection.find({}).sort({ recordId: 1 }).toArray(function (err, result) {
+        if (err) throw err;
+        res.status(200).send(result);
+    })
+});
+
+// Post the info
+router.post('/', function (req, res) {
+    const collection = client.db("uidd-db").collection("uidd");
+    collection.countDocuments(function (err, count) {
+        collection.find({}).sort({ recordId: 1 }).toArray(function (err, result) {
+            const id = 0;
+            if (count != 0) {
+                id = result[count - 1].recordId;
+            }
+            const postData = {
+                recordId: id + 1,
+                name: req.body.name,
+                number: req.body.number
+            };
+            collection.insertOne(postData, function (err, res) {
+                if (err) throw err;
+                console.log('1 document inserted.');
+            })
+            res.status(201).send('Add name: ' + postData['name'] + ', number: ' + postData['number'] + ' into db Successfully!');
+        })
+    })
+});
+
+// PUT to update certain row update info
+router.put('/', function (req, res) {
+    var putFilter = {
+        recordId: req.body.id
+    };
+    var putData = {
+        $set: {
+            name: req.body.name,
+            number: req.body.number
+        }
+    };
+    const collection = client.db("uidd-db").collection("uidd");
+    collection.updateOne(putFilter, putData, function (err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+    })
+
+    res.status(201).send('Got a PUT request at /record');
+});
+
+// DELETE certain row
+router.delete('/', function (req, res) {
+    var deleteFilter = {
+        recordId: req.body.id
+    };
+    const collection = client.db("uidd-db").collection("uidd");
+    collection.deleteOne(deleteFilter, (err, result) => {
+        console.log(req.body.id, deleteFilter, result.result.n);
+    })
+    res.status(201).send('Delete row: ' + req.body.id + ' from db Successfully!');
+});
+
+module.exports = router;
