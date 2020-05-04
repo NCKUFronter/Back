23a05ec7c284809e9daf145c8dd3bf6fee8e0c60
Kevin <tingyi.chen0825@gmail.com>
2020-05-04 17:09:54 +0800

    invitaion send/receive added

--- routes/invitaion-receive.js
+++ routes/invitaion-receive.js
@@ -7,7 +7,7 @@ router.post("/",
   validatePipe("body", InvitationSchema),
   loginCheck(collections.invitation),
   function (req, res) {
-      answerInvitation(collections.invitation, req.body);
-      res.status(200).send("");
+    answerInvitation(collections.invitation, req.body);
+    res.status(200).send("");
   }
 );
